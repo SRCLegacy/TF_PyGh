c75fc579edf517209d8006bc1f61cd0a348416a0
SRC <rams.zar@gmail.com>
2020-05-20 16:21:06 -0500

    One Line Add

--- demo.py
+++ demo.py
@@ -1,5 +1,18 @@
-print("Hello")
-print("Welcome to Telusko")
-print("test 01")
+"""Ejercicio filtrado de muros - Dynamo+Phyton for Revit v2021.
+Para ser usado en la consola de Python En Dynamo
+Saul Ramos C., May. 2020"""
+# %% Imports
+import clr
 
+clr.AddReference('RevitAPI')
+from Autodesk.Revit.DB import *
 
+clr.AddReference('RevitServices')
+import RevitServices
+from RevitServices.Persistence import DocumentManager
+
+doc = DocumentManager.Instance.CurrentDBDocument
+
+colector = FilteredElementCollector(doc,doc.ActiveView.Id)
+
+OUT = colector.OfCategory(BuiltInCategory.OST_Walls).ToElements()
